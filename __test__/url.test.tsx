b1fcbadb5cc8fa708bf6854b5c3602305e32c693
Lninn <thl384458870@outlook.com>
2023-11-21 10:50:25 +0800

    mock data

--- __test__/url.test.tsx
+++ __test__/url.test.tsx
@@ -1,11 +1,11 @@
 import {
   getIconUrl,
   getSiteDesc,
-  queryText,
   getSiteTitle,
   getSiteKeyword,
 } from "../lib/url"
 import "isomorphic-fetch"
+import { music163Doc } from "./mock"
 
 // ref regex101 https://regex101.com/
 
@@ -36,25 +36,16 @@ test("getIconUrl zhipin", async () => {
 })
 
 test("getSiteDesc 163.music",async () => {
-  const url = "https://music.163.com"
-  const text = await queryText(url)
-
-  const value = getSiteDesc(text)
+  const value = getSiteDesc(music163Doc)
   expect(value).toBe("网易云音乐是一款专注于发现与分享的音乐产品，依托专业音乐人、DJ、好友推荐及社交功能，为用户打造全新的音乐生活。")
 })
 
 test("getSiteTitle 163.music",async () => {
-  const url = "https://music.163.com"
-  const text = await queryText(url)
-
-  const value = getSiteTitle(text)
+  const value = getSiteTitle(music163Doc)
   expect(value).toBe("网易云音乐")
 })
 
 test("getSiteKeyword 163.music",async () => {
-  const url = "https://music.163.com"
-  const text = await queryText(url)
-
-  const value = getSiteKeyword(text)
+  const value = getSiteKeyword(music163Doc)
   expect(value).toBe("网易云音乐，音乐，播放器，网易，下载，播放，DJ，免费，明星，精选，歌单，识别音乐，收藏，分享音乐，音乐互动，高音质，320K，音乐社交，官网，music.163.com")
 })
