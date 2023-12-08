3adb9fb3ec5c381004873b4e41dbcfdf0dfc8955
Lninn <thl384458870@outlook.com>
2023-12-08 20:43:47 +0800

    update

--- app/todo/List.tsx
+++ app/todo/List.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import clsx from "clsx"
-import { SVGProps, useEffect, useState } from "react"
+import { SVGProps, useEffect, useRef, useState } from "react"
 
 interface Item {
   text: string
@@ -15,17 +15,10 @@ export default function List() {
 
   useEffect(() => {
     setLoading(true)
-
-    setTimeout(() => {
-
-      fetch("/api/todo").then(res => res.json()).then(res => {
-        // console.log("debug ", res)
-        setList(res.data)
-        
-      }).finally(() => setLoading(false))
-      
-    }, 3000)
-    
+    fetch("/api/todo").then(res => res.json()).then(res => {
+      // console.log("debug ", res)
+      setList(res.data)
+    }).finally(() => setLoading(false))
   }, [])
 
   function onClick(item: Item) {
@@ -42,28 +35,95 @@ export default function List() {
   return (
     <div className="w-full flex flex-col gap-2">
       {list.map((d, i) => {
-        const cls = clsx(
-          "flex items-center gap-3 bg-[#061955] px-4 py-3 rounded-lg",
-          {
-            "line-through decoration-2 decoration-white": d.checked
-          }
-        )
-
-        return (
-          <div className={cls} key={i} onClick={() => onClick(d)}>
-            {
-              d.checked ? (
-                <div className="border-2 rounded-full w-6 h-6 flex items-center justify-center">
-                  <IcSharpCheck className="text-[14px] text-white" />
-                </div>
-              ): (
-                <div className="border-2 border-[#ff764d] w-6 h-6 rounded-full" style={{ borderColor: d.fill }}></div>
-              )
-            }
-            <div className="text-lg text-white">{d.text}</div>
-          </div>
-        )
+        return <TodoItem key={i} config={d} onClick={() => onClick(d)} />
       })}
+    </div>
+  )
+}
+
+interface ITodoProps {
+  config: Item
+  onClick: () => void
+}
+
+function calculateOpacity(dragDistance, totalWidth) {
+  // 确保拖动距离不小于0且不大于总宽度
+  dragDistance = Math.max(0, Math.min(dragDistance, totalWidth));
+
+  // 计算透明度值
+  const opacity = dragDistance / totalWidth;
+  return opacity;
+}
+
+function TodoItem(props: ITodoProps) {
+  const { config, onClick } = props
+
+  const draggableRef = useRef<HTMLDivElement>(null)
+
+  const [applyX, setApplyX] = useState(0)
+
+  useEffect(() => {
+    const draggable = draggableRef.current
+    if (!draggable) return
+
+    const state = {
+      offsetX: 0,
+      offsetY: 0,
+    }
+
+    draggable.addEventListener("touchstart", function (e) {
+      // 记录初始位置和 left 值
+      const touch = e.touches[0];
+      state.offsetX = touch.clientX - draggable.getBoundingClientRect().left;
+      state.offsetY = touch.clientY - draggable.getBoundingClientRect().top;
+    });
+
+    draggable.addEventListener("touchmove", function (e) {
+      e.preventDefault(); // 防止滚动
+  
+      const touch = e.touches[0];
+  
+      // 计算新的位置
+      const newX = touch.clientX - state.offsetX;
+      // const newY = touch.clientY - state.offsetY;
+  
+      // 更新元素的位置
+      draggable.style.left = newX + "px";
+      // draggable.style.top = newY + "px";
+
+      setApplyX(newX)
+    });
+  }, [])
+
+  return (
+    <div className="relative h-[52px]">
+      <div
+       className="absolute top-0 left-0 right-0 bg-white h-[52px] w-full" 
+        style={{ opacity: calculateOpacity(Math.abs(applyX), 311) }}
+      >
+        <button>删除</button>
+      </div>
+      <div
+        className={clsx(
+          "flex items-center gap-3 bg-[#061955] px-4 py-3 rounded-lg w-full absolute h-[52px]",
+          {
+            "line-through decoration-2 decoration-white": config.checked
+          }
+        )} 
+        onClick={onClick}
+        ref={draggableRef}
+      >
+        {
+          config.checked ? (
+            <div className="border-2 rounded-full w-6 h-6 flex items-center justify-center">
+              <IcSharpCheck className="text-[14px] text-white" />
+            </div>
+          ): (
+            <div className="border-2 border-[#ff764d] w-6 h-6 rounded-full" style={{ borderColor: config.fill }}></div>
+          )
+        }
+        <div className="text-lg text-white">{config.text}</div>
+      </div>
     </div>
   )
 }
