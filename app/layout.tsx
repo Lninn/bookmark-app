ab1ad53f321ba5be9259385065baa0cf3819c3f2
Lninn <thl384458870@outlook.com>
2023-12-08 21:53:02 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout(props: RootLayoutProps) {
   const { children } = props
   return (
     <html lang="en">
-      <body className="overflow-hidden w-screen h-screen">
+      <body className="w-screen h-screen">
         {children}
       </body>
     </html>
